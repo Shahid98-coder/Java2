d82e43154a53381f5bfebac4772f2dd4ce3e81e1
Shahid98-coder <80423075+Shahid98-coder@users.noreply.github.com>
2023-01-20 17:10:46 +0530

    Update hello.java

--- practical/hello.java
+++ practical/hello.java
@@ -1,6 +1,12 @@
+class My{
+	public static void display(){
+		System.out.println("Hi...");
+	}
+}
 class hello{
 
   public static void main(String args[]){
        System.out.println("Its my first program.");
+	My.display();
   }
 }
