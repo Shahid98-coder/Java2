6206db6daea0f2ae0b0d3a7432270ddaefcc38a3
Shahid98-coder <80423075+Shahid98-coder@users.noreply.github.com>
2023-01-24 17:53:23 +0530

    Add files via upload

---
+++ Assignment/TernaryExample.java
@@ -0,0 +1,11 @@
+public class TernaryExample{
+	
+	public static void main(String args[]){
+		int a=2;  
+		int b=5;  
+		int c =(a<b)?a:b; 
+		int d =(a>b)?a:b;		
+		System.out.println(c);
+		System.out.println(c);
+	}
+}  
---
+++ Assignment/comparisonExample.java
@@ -0,0 +1,19 @@
+public class comparisonExample{
+	public void calc(){
+		int a = 10;
+		int b = 5;
+		
+		System.out.println("a>b :"+(a>b));
+		System.out.println("a>b :"+(a<b));
+		System.out.println("a>b :"+(a>=b));
+		System.out.println("a>b :"+(a<=b));
+		System.out.println("a>b :"+(a==b));
+		System.out.println("a>b :"+(a!=b));
+		System.out.println((a>b)&&(a<b));
+		System.out.println((a>b)||(a<b));
+	}
+	public static void main(String arg[]){
+		comparisonExample c = new comparisonExample();
+		c.calc();
+	}
+}
