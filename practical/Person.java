76e43620fa92c95ff4062b1d5a298c0818c0b070
Shahid98-coder <80423075+Shahid98-coder@users.noreply.github.com>
2023-02-23 15:25:18 +0530

    Add files via upload

---
+++ practical/Person.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ADMIN
+ *
+ */
+public class Person {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
