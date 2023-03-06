91745f61c35972fdd8a78a88940dda70f0a4db85
Shahid98-coder <80423075+Shahid98-coder@users.noreply.github.com>
2023-03-06 16:22:28 +0530

    Add files via upload

---
+++ lab/DrawCircle/Shape.java
@@ -0,0 +1,12 @@
+package com.cognizant.geometry;
+
+import com.cognizant.shapes.Circle; 			// to invoke the circle class in this Shape class we have to import this .
+
+public class Shape {			//defining the shape class 
+
+	public static void main(String[] args) { 
+		 @SuppressWarnings("unused")
+		Circle c = new Circle(); 			// by creating the object of circle class with the default constructor Circle()
+
+	}
+}
